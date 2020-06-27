02728f755eda3f3809fc4d73ffa61094dc988727
MarigonaN <marigonanuhija@gmail.com>
2020-06-28 00:41:20 +0200

    commit

--- recap/src/server.js
+++ recap/src/server.js
@@ -1,8 +1,11 @@
 const express = require("express")
+const studentsRouter = require('./students')
 
 const server = express()
 
 const port = process.env.PORT || 3000
+
+server.use
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 
