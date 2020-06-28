93c5baed1c9ae2d581e0f91925fec75bcbd332cf
MarigonaN <marigonanuhija@gmail.com>
2020-06-28 10:44:34 +0200

    new commit

--- recap/src/server.js
+++ recap/src/server.js
@@ -1,11 +1,15 @@
 const express = require("express")
+const listEndpoints = require("express-list-endpoints")
 const studentsRouter = require('./students')
 
 const server = express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3002
 
-server.use
+server.use("/students", studentsRouter)
+
+console.log(listEndpoints(server))
+
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 
--- recap/src/students/index.js
+++ recap/src/students/index.js
@@ -1,5 +1,23 @@
 const express = require("express")
+
+
 const studentsRouter = express.Router()
+
+studentsRouter.get("/", (req, res) => {
+    res.send("ok")
+} )
+studentsRouter.get("/:id", (req, res) => {
+    res.send("ok")
+})
+studentsRouter.post("/", (req, res) => {
+    res.send("ok")
+})
+studentsRouter.put("/:id", (req, res) => {
+    res.send("ok")
+})
+studentsRouter.delete("/:id", (req, res) => {
+    res.send("ok")
+})
 
 
 module.exports = studentsRouter
