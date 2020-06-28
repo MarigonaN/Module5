86bfee004f6effeca3d4adf517e755a00a6a4d03
MarigonaN <marigonanuhija@gmail.com>
2020-06-28 13:35:44 +0200

    commit

--- recap/src/students/index.js
+++ recap/src/students/index.js
@@ -1,17 +1,50 @@
 const express = require("express")
-
+const path = require("path")
+const readFile = require("../utilities")
+const { request, response } = require("express")
 
 const studentsRouter = express.Router()
+const studentsFilePath = path.join(__dirname, "students.json")
+
+
 
 studentsRouter.get("/", (req, res) => {
-    res.send("ok")
-} )
-studentsRouter.get("/:id", (req, res) => {
-    res.send("ok")
+    try{
+    const arrayOfStudents = readFile(studentsFilePath)
+    if(arrayOfStudents.length > 0){
+        res.send(arrayOfStudents)
+    }else{
+        res.status(404).send("no students found")
+    }
+}catch(error){
+    res.status(500).send("No file found")
+}  
 })
+
+
+
+
+studentsRouter.get("/:id", (req, res) => {
+    const arrayOfStudents = readFile(studentsFilePath)
+   const studentFound = arrayOfStudents.find(  //we can also use the filter (but filter return us an array)
+       student => student.id === req.params.id)
+       console.log(studentFound)
+       if(!studentFound){
+        res.status(404).send(`student with id ${req.params.id} not found!`)
+       }else{
+    res.send(studentFound)
+       }
+})
+
+
+
+
 studentsRouter.post("/", (req, res) => {
     res.send("ok")
 })
+
+
+
 studentsRouter.put("/:id", (req, res) => {
     res.send("ok")
 })
